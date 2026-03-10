edf1a3a8f90a87d1e09f9b9eec3fb1243c84bce5
JitenRaj <ji10raj@gmail.com>
2026-03-10 22:43:52 +0530

    Add: implement url entity

---
+++ src/main/java/com/jitenraj/urlshortener/entity/UrlEntity.java
@@ -0,0 +1,42 @@
+package com.jitenraj.urlshortener.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "urls", indexes = {
+        @Index(name = "idx_short_code", columnList = "shortCode", unique = true)
+})
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class UrlEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 2048)
+    private String url;
+
+    @Column(nullable = false, unique = true, length = 7)
+    private String shortCode;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer accessCount = 0;
+}
