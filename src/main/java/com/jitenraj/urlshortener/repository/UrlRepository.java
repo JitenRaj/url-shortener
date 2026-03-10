2b18392c1da88781836e243af4baaef8446b5bef
JitenRaj <ji10raj@gmail.com>
2026-03-10 22:45:31 +0530

    Add: implement url repository

---
+++ src/main/java/com/jitenraj/urlshortener/repository/UrlRepository.java
@@ -0,0 +1,15 @@
+package com.jitenraj.urlshortener.repository;
+
+import com.jitenraj.urlshortener.entity.UrlEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+
+    Optional<UrlEntity> findByShortCode(String shortCode);
+
+    boolean existsByShortCode(String shortCode);
+}
